142f88c7bbbe69f464268a8f85581b8fa43f1295
Ryan Christian <rchristian@ryanchristian.dev>
2024-02-12 23:56:46 -0600

    docs: Add comment for prerender impl

--- src/index.jsx
+++ src/index.jsx
@@ -29,6 +29,8 @@ if (typeof window !== 'undefined') {
 	hydrate(<App />, document.getElementById('app'));
 }
 
+// More or less copied straight from `preact-iso`:
+// https://github.com/preactjs/preact-iso/blob/6671855b42704a725d7579e43b255d6538cd0ea8/src/prerender.js#L18
 export async function prerender(data) {
     const maxDepth = 10;
     let tries = 0;
